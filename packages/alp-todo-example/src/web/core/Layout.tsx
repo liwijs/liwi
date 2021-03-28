781f417d25421395a4993cbaeab12793c0f093ac
Christophe Hurpeau <christophe@hurpeau.com>
2021-03-28 23:32:03 +0200

    chore: update example

--- packages/alp-todo-example/src/web/core/Layout.tsx
+++ packages/alp-todo-example/src/web/core/Layout.tsx
@@ -1,22 +1,19 @@
 import { Helmet, AppContainer } from 'alp-react';
-import { hot } from 'alp-react/hot';
 import type { ReactElement } from 'react';
 import React from 'react';
 import './Layout.global.scss';
 import App from './App';
 
-export default hot(
-  (): ReactElement => {
-    return (
-      <AppContainer>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
+export default function Layout(): ReactElement {
+  return (
+    <AppContainer>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
 
-          {/* <link rel="icon" type="image/png" href={favicon} /> */}
-        </Helmet>
-        <App />
-      </AppContainer>
-    );
-  },
-);
+        {/* <link rel="icon" type="image/png" href={favicon} /> */}
+      </Helmet>
+      <App />
+    </AppContainer>
+  );
+}
