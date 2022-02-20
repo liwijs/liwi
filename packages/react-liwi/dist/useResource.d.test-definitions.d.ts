5714c5b06ea4c2df8ea6059f2c28b9773f237d59
Christophe Hurpeau <christophe@hurpeau.com>
2022-02-20 22:14:25 +0100

    chore: dist

---
+++ packages/react-liwi/dist/useResource.d.test-definitions.d.ts
@@ -0,0 +1,5 @@
+export declare function TestDataNotUndefinedWhenLoadingTrue(): void;
+export declare function TestDataNotUndefinedWhenLoadingTrueWhenDestructuring(): void;
+export declare function TestParamsUndefined(): void;
+export declare function TestParamsShouldBeRequired(): void;
+//# sourceMappingURL=useResource.d.test-definitions.d.ts.map
