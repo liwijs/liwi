35260178289ef999f64ef7dd62d4eee62701f688
Christophe Hurpeau <christophe@hurpeau.com>
2022-03-19 16:21:53 +0100

    refactor: ignore last eslint error in remix app

--- @todo-example/remix/app/components/Main.tsx
+++ @todo-example/remix/app/components/Main.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import type { Task } from '@todo-example/modules';
 import type { ReactElement } from 'react';
 import { useContext, useMemo, useState } from 'react';
