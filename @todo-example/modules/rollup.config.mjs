f34cee860f968b0dcc79b20d55d9e2b08dd1c04a
Christophe Hurpeau <christophe@hurpeau.com>
2023-07-29 19:01:19 +0200

    fix: fix modules config

--- @todo-example/modules/rollup.config.mjs
+++ @todo-example/modules/rollup.config.mjs
@@ -1,12 +1,8 @@
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import createRollupConfig from 'pob-babel/createRollupConfig.js';
-import run from 'pob-babel/plugin-run.cjs';
-
-const watch = process.env.ROLLUP_WATCH === 'true';
 
 export default createRollupConfig({
   cwd: dirname(fileURLToPath(import.meta.url)),
   outDirectory: 'build',
-  plugins: [watch && run({ execArgv: ['--enable-source-maps'] })],
 });
