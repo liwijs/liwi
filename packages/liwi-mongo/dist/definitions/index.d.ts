6b50d27ddeeadbeb78a1fa660cc6987cd448082c
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2025-08-24 15:34:17 +0200

    feat!: drop node 20

--- packages/liwi-mongo/dist/definitions/index.d.ts
+++ packages/liwi-mongo/dist/definitions/index.d.ts
@@ -1,16 +1,6 @@
 export type { MongoBaseModel, MongoKeyPath, MongoInsertType, } from "./MongoBaseModel";
 export type { Update } from "liwi-store";
-<<<<<<< HEAD
-export { default as MongoStore } from "./MongoStore";
-export { default as MongoConnection, type MongoConfig, } from "./MongoConnection";
-export { default as createMongoSubscribeStore, type SubscribeStore, } from "./createMongoSubscribeStore";
-||||||| parent of 0b24891b (feat!: update dependencies, change config from map to object)
-export { default as MongoStore } from "./MongoStore";
-export { default as MongoConnection } from "./MongoConnection";
-export { default as createMongoSubscribeStore } from "./createMongoSubscribeStore";
-=======
 export { default as MongoStore } from "./MongoStore.ts";
 export { default as MongoConnection, type MongoConfig, } from "./MongoConnection.ts";
 export { default as createMongoSubscribeStore, type SubscribeStore, } from "./createMongoSubscribeStore.ts";
->>>>>>> 0b24891b (feat!: update dependencies, change config from map to object)
 //# sourceMappingURL=index.d.ts.map
